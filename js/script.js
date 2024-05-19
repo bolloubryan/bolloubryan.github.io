631d958cd02be4d6ffd01135ccc82795c80d4bbd
Bryan Bollou <56371334+bolloubryan@users.noreply.github.com>
2024-05-19 16:51:36 -0400

    Update script.js
    testing out the sortable js an dynamically making a list

--- js/script.js
+++ js/script.js
@@ -1,4 +1,18 @@
-function tellTime (){
-	let d = new Date()
-	document.getElementById("message").innerHTML = d;
-}
+const sortablelist = document.getElementById('sortablelist')
+
+new Sortable(sortablelist, {
+   animation: 150,
+   ghostClass: 'sortable-ghost'
+ });
+
+
+let ct = document.querySelector(".sortable");
+
+const arr = ["naruto","one piece","hxh"];
+
+
+arr.forEach(value=>{
+  const currentElement = document.createElement("li");
+  currentElement.innerHTML = value + "<br/>";
+  ct.appendChild(currentElement);
+})
